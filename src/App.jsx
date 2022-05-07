a300c85fb3d8cce2fc55fd61a3a4d74a7ee14828
Diego Acosta <diegoacosta211@gmail.com>
2022-05-07 00:43:44 -0300

    react-router & details page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import SearchParams from './SearchParams';
+import Details from './Details';
 
 const App = () => (
+  <BrowserRouter>
     <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
+      <header>
+        <Link to="/">
+          <h1>Adopt Me!</h1>
+        </Link>
+      </header>
+      <Routes>
+        <Route path="/" element={<SearchParams/>} />
+        <Route path="/details/:id" element={<Details/>} />
+      </Routes>
     </div>
-  )
+  </BrowserRouter>
+);
 
 export default App;
