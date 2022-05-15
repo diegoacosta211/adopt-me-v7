3fc948298587460fe2347e8b5e36ed2d0b4134ff
Diego Acosta <diegoacosta211@gmail.com>
2022-05-15 12:58:06 -0300

    add typescript

---
+++ src/Modal.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useRef, FunctionComponent, MutableRefObject, ReactNode } from "react";
+import { createPortal } from "react-dom";
+
+type ModalProps = {
+  children: ReactNode
+}
+
+const Modal: FunctionComponent<ModalProps> = ({ children }) => {
+  const modalRef: MutableRefObject<HTMLDivElement | null> = useRef(null);
+  if (!modalRef.current) {
+    modalRef.current = document.createElement('div');
+  }
+
+  useEffect(() => {
+    const modalRoot = document.getElementById('modal');
+    // defensive programming
+    if (!modalRoot || !modalRef.current) {
+      return;
+    }
+    modalRoot.appendChild(modalRef.current);
+    return () => {
+      if (modalRef.current) {
+        modalRoot.removeChild(modalRef.current);
+      }
+    }
+  }, []);
+
+  return createPortal(<div className="modal">{children}</div>, modalRef.current);
+};
+
+export default Modal;
